7d86cfb59d66455905ddbb1e3f21cd590fc9cbf2
Ahmad Arbain <49236309+ahmadarbain@users.noreply.github.com>
2023-02-12 00:26:35 +0700

    last update

--- Week_2/flows/02_gcp/etl_web_to_gcs.py
+++ Week_2/flows/02_gcp/etl_web_to_gcs.py
@@ -16,8 +16,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task()
 def clean(df = pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
+    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"row: {len(df)}")
@@ -38,7 +38,7 @@ def write_gcs(path: Path) -> None:
     return
 
 @flow()
-def etl_web_to_gcs(color:str, month:int, year:int) -> None:
+def etl_web_to_gcs(color:str, month:int, year:int):
     """The main ETL Function"""
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
