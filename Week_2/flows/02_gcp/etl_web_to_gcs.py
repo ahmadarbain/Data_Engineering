5a04a62a2711153b1e29119994fa9ca2e9b1f9d4
Ahmad Arbain <49236309+ahmadarbain@users.noreply.github.com>
2023-02-12 00:01:20 +0700

    Update etl_web_to_gcs.py

--- Week_2/flows/02_gcp/etl_web_to_gcs.py
+++ Week_2/flows/02_gcp/etl_web_to_gcs.py
@@ -2,54 +2,54 @@ import pandas as pd
 from pathlib import Path
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
+from prefect_gcp import GcpCredentials
 
 @task(retries=3)
-def fetch(dataset_url: str) -> pd.DataFrame:
-    """Read taxi data from web into pandas DataFrame"""
-    
-    # if randint(0, 1) > 0:
-    #     raise Exception
-    df = pd.read_csv(dataset_url, low_memory=False)
-    return df
-
-@task(log_prints=True)
-def clean(df = pd.DataFrame) -> pd.DataFrame:
-    """Fix dtype issues"""
-    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
-    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
-    print(df.head(2))
-    print(f"columns: {df.dtypes}")
-    print(f"row: {len(df)}")
-    return df
-
-@task(log_prints=True)
-def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
-    """Write Dataframe out locally as parquet file"""
-    path =  Path(f"Week_2/data/{color}/{dataset_file}.parquet").as_posix()
-    df.to_parquet(path, compression="gzip")
-    return path
-
-@task(log_prints=True)
-def write_gcs(path: Path) -> None:
-    """Upload local parquet file to GCS"""
+def extract_from_gcs(color: str, year: int, month: str):
+    """Doownload trip data from GCS"""
+    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("de-zoomcamp")
-    gcs_block.upload_from_path(from_path=path, to_path=path, timeout=90000)
-    return
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"")
+    return Path(f"{gcs_path}")
+
+
+@task(log_prints=True)
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    df = pd.read_parquet(path)
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    df["passenger_count"].fillna(0, inplace=True)
+    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
+
+@task(log_prints=True)
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to Big Query"""
+    gcp_credentials_block = GcpCredentials.load("de-zcamp")
+
+    df.to_gbq(
+        destination_table="dezoomcamp.rides",
+        project_id="bionic-kiln-342115",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append"
+    )
 
 @flow()
-def etl_web_to_gcs() -> None:
-    """The main ETL Function"""
-    color = "green"
-    year = 2020
-    month = 11
-    dataset_file = f"{color}_tripdata_{year}-{month:02}"
-    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+def etl_gcs_to_bq(
+    color: str,
+    year: int,
+    month: int
+):
+    """Main ETL flow to load data into Big Query"""
 
-    df = fetch(dataset_url)
-    df_clean = clean(df)
-    path =  write_local(df_clean, color, dataset_file)
-    write_gcs(path) 
-
-if __name__ == '__main__':
-    etl_web_to_gcs() 
+    path = extract_from_gcs(color, year, month)
+    df = transform(path)
+    write_bq(df)
+    
+    
+if __name__ == "__main__":
+    color:str = "green"
+    year:int = 2019
+    month:int = 4
+    etl_gcs_to_bq(color, year, month)
