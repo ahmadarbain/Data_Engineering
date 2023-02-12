d04ad4c1140b5a3b8b95363e8fda0ada131e63a0
Ahmad Arbain <49236309+ahmadarbain@users.noreply.github.com>
2023-02-12 14:46:17 +0700

    Update etl_web_to_gcs.py

--- Week_2/flows/02_gcp/etl_web_to_gcs.py
+++ Week_2/flows/02_gcp/etl_web_to_gcs.py
@@ -33,7 +33,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
 @task()
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("de-zoomcamp")
+    gcs_block = GcsBucket.load("de-zoomcamp", validate=False)
     gcs_block.upload_from_path(from_path=path, to_path=path, timeout=90000)
     return
 
