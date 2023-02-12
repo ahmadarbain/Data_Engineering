c6b61a8cef90935d219a934ad9b7c151402041f9
Ahmad Arbain <49236309+ahmadarbain@users.noreply.github.com>
2023-02-12 17:44:50 +0700

    Update etl_web_to_gcs.py

--- Week_2/flows/02_gcp/etl_web_to_gcs.py
+++ Week_2/flows/02_gcp/etl_web_to_gcs.py
@@ -40,7 +40,7 @@ def write_gcs(path: Path) -> None:
 @flow()
 def etl_web_to_gcs(year:int, color:str, months:int):
     """The main ETL Function"""
-    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_file = f"{color}_tripdata_{year}-{months:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     df = fetch(dataset_url)
